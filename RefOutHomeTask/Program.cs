3acc1c37224287c7763f866759dda761dd36399e
AykhanTa <aykhanft@code.edu.az>
2024-02-25 18:10:46 +0400

    Some tasks

--- RefOutHomeTask/Program.cs
+++ RefOutHomeTask/Program.cs
@@ -13,9 +13,17 @@
 
             //Console.WriteLine(IsPalindrome("atat"));
 
+            //string word=Console.ReadLine();
+            //Console.WriteLine(RemoveRepeatedChar(word));
+
+            //string word = Console.ReadLine();
+            //ReversedString(word);
+
+            string word = "Cavid yaxsi oglandi";
+            SplitString(word);
 
         }
-        static  void MinusElementsArr(int[] arr)
+        static void MinusElementsArr(int[] arr)
         {
             for (int i = 0; i < arr.Length; i++)
             {
@@ -23,13 +31,13 @@
                 {
                     arr[i] *= -1;
                 }
-                Console.Write(arr[i]+" ");
+                Console.Write(arr[i] + " ");
             }
         }
 
         static void Repeat(string word, int count)
         {
-            for (int i = 0; i <count; i++)
+            for (int i = 0; i < count; i++)
             {
                 Console.Write(word);
             }
@@ -37,7 +45,7 @@
 
         static bool IsPalindrome(string s)
         {
-            string newString=string.Empty;
+            string newString = string.Empty;
             for (int i = s.Length - 1; i >= 0; i--)
             {
                 newString += s[i];
@@ -51,6 +59,56 @@
                 return false;
             }
         }
+        static string RemoveRepeatedChar(string word)
+        {
+            string newString = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                bool repeated = false;
+                for (int j = 0; j < newString.Length; j++)
+                {
+                    if (word[i] == newString[j])
+                    {
+                        repeated = true;
+                        break;
+                    }
+                }
+                if (!repeated)
+                {
+                    newString += word[i];
+                }
+            }
+            return newString;
+        }
 
+        static void ReversedString(string word)
+        {
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                Console.Write(word[i]);
+            }
+        }
+
+        static void SplitString(string word)
+        {
+            string newString = string.Empty; 
+            word += " ";
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != ' ')
+                {
+                    newString += word[i];
+                }
+                else
+                {
+                    if (newString.Length>0)
+                    {
+                        Console.WriteLine(newString);
+                        newString = "";
+                    }
+                }
+            }
+        }
     }
 }
